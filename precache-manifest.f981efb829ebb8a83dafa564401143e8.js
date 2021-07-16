eacba7ceebe8853ed165828f0ff521ac0401fd08
JAKHAR-23 <sunny.jakhar.cse.2017@miet.ac.in>
2021-07-16 23:16:17 +0530

    Updates

---
+++ precache-manifest.f981efb829ebb8a83dafa564401143e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcab4c8c8455e8f7026c25bb9fadcd9",
+    "url": "/cyberfolks/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/cyberfolks/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "84768b9bff1dedbc5074",
+    "url": "/cyberfolks/static/css/main.82559137.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/cyberfolks/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyberfolks/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84768b9bff1dedbc5074",
+    "url": "/cyberfolks/static/js/main.1a60c244.chunk.js"
+  },
+  {
+    "revision": "2f65892c85efe5ee97e2",
+    "url": "/cyberfolks/static/js/runtime-main.7a81ff50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cyberfolks/static/media/SEGA.12a3db8b.woff"
+  }
+]);
